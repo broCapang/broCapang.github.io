c5a56e529a980f74e73f1f60384bc5ef160ff25e
broCapang <U2000429@siswa.um.edu.my>
2024-01-07 18:39:12 +0900

    Update contact page layout and styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,26 +3,27 @@ import { MdEmail } from 'react-icons/md';
 
 const ContactPage = () => {
     return (
-        <main className="flex-grow container mx-auto">
-        <h1 className="text-2xl font-bold text-white p-10">Hey There</h1>
-        <h1 className="text-xl font-bold text-white p-10">Feel free to contact me!</h1>
-        
-        <div className="grid grid-cols-3 gap-4 p-10">
-          <a href="https://www.linkedin.com/in/gnapaC/" 
-             className="text-white hover:text-slate-300 text-2xl">
-            <FaLinkedin className="inline-block mr-2" />LinkedIn
-          </a>
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-xl sm:text-2xl font-bold text-white p-5">Hey There</h1>
+          <h1 className="text-lg sm:text-xl font-bold text-white p-5">Feel free to contact me!</h1>
+          
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-5">
+              <a href="https://www.linkedin.com/in/gnapaC/" 
+                className="text-white hover:text-slate-300 text-xl sm:text-2xl">
+                  <FaLinkedin className="inline-block mr-2" />LinkedIn
+              </a>
 
-          <a href="https://github.com/broCapang" 
-             className="text-white hover:text-slate-300 text-2xl">
-            <FaGithub className="inline-block mr-2" />GitHub
-          </a>
-          <a href="mailto:ihaziq18.ih@gmail.com" 
-             className="text-white hover:text-slate-300 text-2xl">
-            <MdEmail className="inline-block mr-2" />Email
-          </a>
-        </div>
+              <a href="https://github.com/broCapang" 
+                className="text-white hover:text-slate-300 text-xl sm:text-2xl">
+                  <FaGithub className="inline-block mr-2" />GitHub
+              </a>
+              <a href="mailto:ihaziq18.ih@gmail.com" 
+                className="text-white hover:text-slate-300 text-xl sm:text-2xl">
+                  <MdEmail className="inline-block mr-2" />Email
+              </a>
+          </div>
       </main>
+
     );
 }
 export default ContactPage;
